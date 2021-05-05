a4ce98c050916aedbc4a645aeae43d77ac33e92f
drevanth4u <46389696+drevanth4u@users.noreply.github.com>
2021-05-05 00:34:54 -0500

    added rating-data-service

---
+++ rating-data-service/src/main/java/com/example/demo/RatingResource.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable String movieId) {
+		return new Rating(movieId, 4);
+	}
+	
+
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		
+		List<Rating> ratings = Arrays.asList(
+              new Rating("1234", 4),
+			  new Rating("4567", 5));
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
+	}
+}
